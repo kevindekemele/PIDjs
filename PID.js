dc023604f00f01df19acfadfb143ec534232784a
KevinDekemele <kevindekemele@gmail.com>
2017-07-23 14:28:01 +0200

    Removed Babel to prepare Webpack and Example

--- PID.js
+++ PID.js
@@ -1,26 +1,29 @@
 
-export default class PIDcontroller {
+ // ES6
+
+ class PIDcontroller {
+
   constructor ({P = 1, I = 0, D = 0, dt = 1} = {}) {
-    this.P = P
-    this.I = I
-    this.D = D
-    this.dt = dt
-    this.T = 0
+    this.P = P;
+    this.I = I;
+    this.D = D;
+    this.dt = dt;
+    this.T = 0;
+    this.ie = 0;
+    this.pe=0;
+    this.de=0;
+    this.control_effort = 0;
+    this.prev_error = 0;
   }
 
-  control (state, setpoint) {
-    var pe = setpoint - state
-    var de = 0
-    // derivative error
-    if (this.T !== 0) {
-      de = (pe - this.prev_error) / (this.dt)
-    }
-    // integral error
-    this.ie = this.ie + (this.dt) * pe
-    this.T = this.T + this.dt
-    this.prev_error = pe
-
-    var control = this.P * pe + this.I * this.ie + this.D * de
-    return control
+  control (state, setpoint, time) {
+        this.pe = setpoint - state;
+        this.de = (this.pe - this.prev_error) / (this.dt);
+        this.ie = this.ie + (this.dt) * this.pe;
+        var control = this.P * this.pe + this.I * this.ie + this.D * this.de;
+        this.prev_error = this.pe;
+        return control;
   }
 }
+
+export default PIDcontroller;
